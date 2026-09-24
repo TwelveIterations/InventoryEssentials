44bca78da07ce3fcb80faf86f6e74dcd770a2e60
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-24 21:24:38 +0200

    fix: Fix potential race condition in bundle autofill, tool refill and stack refill

--- common/src/main/java/net/blay09/mods/inventoryessentials/mixin/ClientPacketListenerMixin.java
+++ common/src/main/java/net/blay09/mods/inventoryessentials/mixin/ClientPacketListenerMixin.java
@@ -12,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
 
-    @Inject(method = "handleTakeItemEntity", at = @At("HEAD"))
+    @Inject(method = "handleTakeItemEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/network/PacketProcessor;)V", shift = At.Shift.AFTER))
     private void handleTakeItemEntity(ClientboundTakeItemEntityPacket packet, CallbackInfo callbackInfo) {
         InventoryEssentialsClient.onTakeItemEntityPacket(packet);
     }
 
-    @Inject(method = "handleContainerSetSlot", at = @At("HEAD"))
+    @Inject(method = "handleContainerSetSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/network/PacketProcessor;)V", shift = At.Shift.AFTER))
     private void beforeContainerSetSlot(ClientboundContainerSetSlotPacket packet, CallbackInfo callbackInfo) {
         InventoryEssentialsClient.beforeContainerSetSlotPacket(packet);
     }
